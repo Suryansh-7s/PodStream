15fccaee7f8fef1cd28a57faca2155e144bcb1c2
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:04:44 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,3 @@
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
-
-const container = styled.div`
-background-color: ${({ theme }) => theme.bg};
-widht: 100%;
-height: 100vh;
-`;
-
-function App(){
-  const [darkMode, setDarkMode] = useState(false);
-  
-  return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    <container>PodStream</container>
-    </ThemeProvider>
-  );
-}
-
-export default App;
