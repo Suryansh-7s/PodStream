ea7042c69fc046c2b221ff054925b4929426a194
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:08:03 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,1 @@
-import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
-import { darkTheme, lightTheme } from './utils/Themes.js'
-import styled from 'styled-components';
-
-function App() {
-
-  const [darkMode, setDarkMode] = useState(true);
-  return (
-
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    </ThemeProvider>
-
-  );
-}
-
-export default App;
+import 
