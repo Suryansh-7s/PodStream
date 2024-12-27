83e2fe9a9ea548ffb64ec18f5b9e27a21a5c0260
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:09:03 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,3 @@
 import { useState } from "react";
 import styled, {ThemeProvider} from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "./utils/themes";
+import { lightTheme, darkTheme, GlobalStyles } from "./utils/Themesthemes";
