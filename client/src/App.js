2c178111fdc5a1419e1d3aa53c03c0a4787f36f9
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:33:04 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const container = styled.div`
 function App() {
   const [darkMode, setDarkMode] = useState(true);
   return (
-    <ThemeProvider theme= 
+    <ThemeProvider theme={}
       <container>
         PodStream
       </container>
