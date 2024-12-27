317f191b9f610071adc705ab6392d562e957c912
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 17:01:30 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Logo = styled.div`
 `;
 const Close = styled.div``;
 const Elements = styled.div``;
-const NavText = styled.div``;idsddd
+const NavText = styled.div``;idsdad
 
 const Sidebar = () => {
   return (
