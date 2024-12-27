8df1933362a6ebabfed32f9e2a1a9508ef661b22
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 15:52:32 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import useState from "react";
 import styled from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
+import { lightTheme, darkTheme } from "./util/theme";
 const container = styled.div`
 
 function App(){
