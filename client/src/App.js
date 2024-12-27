e3bf6d10d6ac9b7e478950ff117039977e5d1dc5
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:31:49 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,6 @@ const container = styled.div`
   background: ${({theme}) => theme.bg};
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background 0.5s;
 `;
 
 function App() {
