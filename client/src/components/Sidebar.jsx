eab3e92b40b7f8c41740940a0e60c488056bd122
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:45:29 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,9 +7,7 @@ import CloseRounded from '@mui/icons-material/CloseRounded';
 const MenuContianer = styled.div``;
 const Logo = styled.div``;
 const Close = styled.div``;
-
 const Elements = styled.div``;
-const HomeRounded = styled.div``;
 const NavText = styled.div``;
 
 const Sidebar = () => {
