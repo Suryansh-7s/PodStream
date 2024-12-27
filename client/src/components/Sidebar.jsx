e5ad70aa1bd0a941b4d8021df8e3b3b80c7df426
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:58:09 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -24,7 +24,6 @@ const Flex = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 20px;
 `;
 const Logo = styled.div`
     color: ${({theme}) => theme.primary};
