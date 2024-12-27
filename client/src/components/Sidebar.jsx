0e571a62f586e39853caa445576186bcb10f2e27
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:51:18 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const MenuContianer = styled.div`
     flex-direction: column;
     height: 100vh;
     display: flex;
-    ali
+    background: ${({theme}) => theme.sidebar};
 `;
 
 const Logo = styled.div``;
