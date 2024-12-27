ad2e0ebd8dab30d6121a134882ccf54cf4818650
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:32:10 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const container = styled.div`
 `;
 
 function App() {
-  const [darkTheme, setDarkMode] = useState("true");
+  const [darkMode, setDarkMode] = useState("true");
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <container>
