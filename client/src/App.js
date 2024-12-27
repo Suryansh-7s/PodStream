e0f75065a773e29303de5d12b4d2079e0a66382b
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 15:57:35 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App(){
   return (    
   <div>
     <ThemeProvider theme={lightTheme}>
-    <container>PodStream</container>
+    <container>PodStream</Container>
     </ThemeProvider>
 
 
