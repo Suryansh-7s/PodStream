68ccab94f5f220f8f51deed03c22c15f8e146784
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:54:09 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ const MenuContianer = styled.div`
         z-index: 1000;
         width: 100%;
         max-width: 250px;
-        left: ${({set
+        left: ${({setMenu
 `;
 
 const Logo = styled.div``;
