337390ee641e8f0c9a6a3e942dce00120af717c9
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 15:53:29 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 import { lightTheme, darkTheme } from "./utils/theme";
 
 const container = styled.div`
-background-color: ${({props => props.theme.backgroundColor};
-
+background-color: ${({ theme }) => theme.body};
 function App(){
   return (
     const [darkMode, setDarkMode] = useState(true);
