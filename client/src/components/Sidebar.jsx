308f477412fd126b60abc3c35e9653ba306105b6
Suryansh-7s <sharmasuryansh2004@gmail.com>
2024-12-27 16:43:56 +0530

    Auto-commit: Changes saved on $(date)

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { HomeRounded,C from '@mui/icons-material/HomeRounded'
+import { HomeRounded,CloseR from '@mui/icons-material/HomeRounded'
 
 const MenuContianer = styled.div``;
 const Logo = styled.div``;
